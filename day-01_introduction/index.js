c819d646ef313e22a0b685721eb164ee0ec460f7
dmystical-coder <dmystical.coder@gmail.com>
2024-01-09 15:51:49 +0100

    :star: feat: declare variables to store bio-data
    Declare variables to store your first name, last name, marital status,
    country and age in multiple lines and a single line.

--- day-01_introduction/index.js
+++ day-01_introduction/index.js
@@ -9,7 +9,7 @@ informative
 
 // declare four variables without assigning values
 let name;
-let age;
+let height;
 let isMale;
 let bodyMassIndex;
 
@@ -18,3 +18,13 @@ const PI = 3.142;
 const GRAVITY = `${9.8}m/s`;
 let isRound = false;
 let author = null;
+
+// declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "Abdulsalam";
+let lastName = "Baruwa";
+let isMarried = false;
+let country = "Nigeria";
+let age = 20;
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line.
+firstName = "Abdulsalam", lastName = "Baruwa", isMarried = false, country = "Nigeria", age = 20;
