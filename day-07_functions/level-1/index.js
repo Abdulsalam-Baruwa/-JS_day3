e3c9cf4220f93a45dcc1c9afc73fde0a55322afb
dmystical-coder <dmystical.coder@gmail.com>
2024-01-10 23:32:58 +0100

    :star: feat: declare function that takes params
    Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.

--- day-07_functions/level-1/index.js
+++ day-07_functions/level-1/index.js
@@ -1,5 +1,10 @@
 // Declare a function fullName and it print out your full name.
 
-const fullName = function () {
-    console.log('Abdulsalam Baruwa');
+// const fullName = function () {
+//     console.log('Abdulsalam Baruwa');
+// }
+
+// Declare a function fullName and now it takes firstName, lastName as a parameter and it returns your full - name.
+const fullName = function (firstName, lastName) {
+    return `${firstName} ${lastName}`;
 }
